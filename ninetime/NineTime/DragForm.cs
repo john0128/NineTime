b266e76f2f513480283118951578441c4d10a0b8
Nine <79080468+john0128@users.noreply.github.com>
2021-08-28 17:10:40 +0900

    Add files via upload

---
+++ ninetime/NineTime/DragForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace NineTime
+{
+    class DragForm
+    {
+        private Control handleControl;
+
+        public Control SelectControl
+        {
+            get { return this.handleControl; }
+            set
+            {
+                this.handleControl = value;
+                this.handleControl.MouseDown += new MouseEventHandler(this.DragForm_MouseDown);
+            }
+        }
+        [DllImport("user32.DLL")]
+        public static extern int SendMessage(IntPtr a, int msg, int wParam, int param);
+        [DllImport("user32.DLL")]
+        public static extern bool ReleaseCapture();
+
+        private void DragForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            bool flag = e.Button == MouseButtons.Left; //좌클:Left 우클:Right 휠클:Middle
+            if (flag)
+            {
+                DragForm.ReleaseCapture();
+                DragForm.SendMessage(this.SelectControl.FindForm().Handle, 161, 2, 0);
+            }
+        }
+    }
+}
---
+++ ninetime/NineTime/EllipseControl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.ComponentModel;
+
+namespace NineTime
+{
+    class EllipseControl : Component
+    {
+        [DllImport("Gdi32.DLL", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(
+            int nLeftRect,
+            int nRightRect,
+            int nTopRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+            );
+        private Control _cntrl;
+        private int _CornerRadius = 30;
+
+        public Control TargetControl
+        {
+            get { return _cntrl; }
+            set
+            {
+                _cntrl = value;
+                _cntrl.SizeChanged += (sender, EventArgs) =>
+                _cntrl.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, _cntrl.Width, _cntrl.Height, _CornerRadius, _CornerRadius));
+            }
+        }
+
+        public int CornerRadius
+        {
+            get { return _CornerRadius; }
+            set
+            {
+                _CornerRadius = value;
+                if (_cntrl != null)
+                    _cntrl.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, _cntrl.Width, _cntrl.Height, _CornerRadius, CornerRadius));
+            }
+        }
+    }
+}
